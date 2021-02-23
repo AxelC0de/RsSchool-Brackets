184527b309421eb89c6fd8c47cf699d22b724d7e
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-02-23 04:35:43 +0300

    .

---
+++ app.js
@@ -0,0 +1,42 @@
+check('())(', [['(', ')']])
+
+check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']])
+
+
+function check(str, bracketsConfig) {
+	//сделать из поданной строки массив
+	let arr = str.split('')
+
+	// Отсечка:
+
+	// есть ли мусор ?:
+
+	// 	сделать из Конфига один массив
+
+	let firstPartConfig = config[0];
+	// console.log(firstPartConfig)
+
+	// config.shift()
+	let secondPartConfig = config;
+	// console.log(secondPartConfig)
+
+	let joinedConfig = firstPartConfig.concat(secondPartConfig);
+	// console.log(joinedConfig)
+
+	let configToString = config.toString()
+	// console.log(configToString);
+
+	let splittedConfig = configToString.split(',');
+	// console.log(splittedConfig);
+
+	//  убрать повторы в этом массиве, тогда сравнить
+
+	// 1) убрать повторы
+	let strToSort = str.split('')
+
+	const strSorted = strToSort.filter((it, index) => index === strToSort.indexOf(it = it.trim()));
+	//2)тогда сравнить
+
+	// strSorted & splittedConfig сравнить, но можно их конкатинировать и удалить повторы, если непустой - то ошибка
+
+}
