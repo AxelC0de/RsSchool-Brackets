c837aa7429538f76b851becee3443220ecc32ed6
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-02-23 12:10:55 +0300

    .

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ function check(str, bracketsConfig) {
 	//сделать из поданной строки массив
 	let arr = str.split('')
 
-	// Отсечка:
 
-	// есть ли мусор ?:
+	// 	1) сделать из Конфига один массив
 
-	// 	сделать из Конфига один массив
-
-	let firstPartConfig = config[0];
+	let firstPartConfig = bracketsConfig[0];
 	// console.log(firstPartConfig)
 
 	// config.shift()
-	let secondPartConfig = config;
+	let secondPartConfig = bracketsConfig;
 	// console.log(secondPartConfig)
 
 	let joinedConfig = firstPartConfig.concat(secondPartConfig);
 	// console.log(joinedConfig)
 
-	let configToString = config.toString()
+	let configToString = bracketsConfig.toString()
 	// console.log(configToString);
 
 	let splittedConfig = configToString.split(',');
 	// console.log(splittedConfig);
 
-	!!!!!!обьединить массивы все, тогда убрать повторы
-	//  убрать повторы в этом массиве, тогда сравнить
 
-	// 1) убрать повторы
-	let strToSort = str.split('')
+	// Отсечка:
 
-	const strSorted = strToSort.filter((it, index) => index === strToSort.indexOf(it = it.trim()));
-	//2)тогда сравнить
+	// есть ли мусор ?:
 
-	// strSorted & splittedConfig сравнить, но можно их конкатинировать и удалить повторы, если непустой - то ошибка
+	// 2) проверить, включает ли Конфиг Массив
+
+	for (let index = 0; index < arr.length; index++) {
+		if (!splittedConfig.includes(arr[index])) {
+			console.log('false')
+		}
+	}
+
 
 }
